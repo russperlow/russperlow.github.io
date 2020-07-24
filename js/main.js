0035d7c6b930004816b14a2629fb8dafbf90e689
Russell Perlow <rperlow@MacBook-Pro.local>
2020-07-24 11:13:11 -0400

    transitions added to timeline, bio updated

--- js/main.js
+++ js/main.js
@@ -34,15 +34,15 @@ const app = new Vue({
 	data: {
 		languages: 'Languages',
 		languageSubs: [
+			{name: 'JavaScript', percentage: '95'},
+			{name: 'HTML & CSS', percentage: '95'},
 			{name: 'C#', percentage: '95'},
 			{name: 'Java', percentage: '95'},
-			{name: 'HTML & CSS', percentage: '95'},
-			{name: 'JavaScript', percentage: '95'},
-			{name: 'C++', percentage:'75'},
+			{name: 'SQL', percentage: '75'},
 			{name: 'Swift', percentage: '75'},
+			{name: 'Kotlin', percentage: '75'},
+			{name: 'C++', percentage:'75'},
 			{name: 'Python', percentage: '75'},
-			{name: 'SQL', percentage: '65'}
-
 		],
 		devTools: 'Developer Tools',
 		devToolSubs: [
@@ -62,3 +62,74 @@ const app = new Vue({
 
 	}
 });
+
+const TIME_OUTS = [0, 1000, 2000, 3000];
+
+function isElementInViewport(elem){
+	var element = $(elem)
+	// window bottom edge
+	var windowBottomEdge = $(window).scrollTop() + $(window).height();
+	
+	// element top edge
+	var elementTopEdge = element.offset().top;
+	var offset = 100;
+
+	// if element is between window's top and bottom edges
+	return elementTopEdge + offset <= windowBottomEdge;
+
+}
+
+function checkAnimationRight(){
+	$('.rightMoveIn').each(function(index){
+		if(isElementInViewport($(this))){
+			if($(this).hasClass('timePeriodRight')){
+				$(this).removeClass('invisible');
+				$(this).addClass('rightMoveInStart')
+			}else if($(this).hasClass('companyRight')){
+				var $element = $(this)
+				setTimeout(function(){addRightAnimationClass($element)}, TIME_OUTS[1])
+			}else if($(this).hasClass('positionRight')){
+				var $element = $(this)
+				setTimeout(function(){addRightAnimationClass($element)}, TIME_OUTS[2])
+			}else if($(this).hasClass('contentRight')){
+				var $element = $(this)
+				setTimeout(function(){addRightAnimationClass($element)}, TIME_OUTS[3])
+			}
+		}
+	})
+}
+
+function checkAnimationLeft(){
+	$('.leftMoveIn').each(function(index){
+		if(isElementInViewport($(this))){
+			if($(this).hasClass('timePeriodLeft')){
+				$(this).removeClass('invisible');
+				$(this).addClass('leftMoveInStart')
+			}else if($(this).hasClass('companyLeft')){
+				var $element = $(this)
+				setTimeout(function(){addLeftAnimationClass($element)}, TIME_OUTS[1])
+			}else if($(this).hasClass('positionLeft')){
+				var $element = $(this)
+				setTimeout(function(){addLeftAnimationClass($element)}, TIME_OUTS[2])
+			}else if($(this).hasClass('contentLeft')){
+				var $element = $(this)
+				setTimeout(function(){addLeftAnimationClass($element)}, TIME_OUTS[3])
+			}
+		}
+	})
+}
+
+function addRightAnimationClass(elem){
+	$(elem).addClass('rightMoveInStart')
+	$(elem).removeClass('invisible')
+}
+
+function addLeftAnimationClass(elem){
+	$(elem).addClass('leftMoveInStart')
+	$(elem).removeClass('invisible')
+}
+
+$(window).scroll(function(){
+	checkAnimationRight()
+	checkAnimationLeft()
+})
